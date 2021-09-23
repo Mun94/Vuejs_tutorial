d5b5a8b5345e557db6176de4c4d1d1eccf6fe17e
Mun94 <seongwoo720@gmail.com>
2021-09-23 16:06:03 +0900

    repactor

--- calculator/src/store/modules/calculationVal.ts
+++ calculator/src/store/modules/calculationVal.ts
@@ -1,4 +1,4 @@
-interface TState {
+interface IState {
     clickVal : string;
     process  : string;
 
@@ -12,13 +12,36 @@ interface TState {
 interface IActions {
     cal: (
         { commit, state }: 
-            { commit: (key: string, val?: number | string) => void, state: TState }, 
+            { commit: (key: string, val?: number | string) => void, state: IState }, 
         payload: 
             { val: string, controller: string }
     ) => void
 };
 
-const state: TState = {
+interface IMutations {
+    PROCESS: (state: IState, payload: number | string) => string;
+    CLICK_VAL: (state: IState, payload: number | string) => string;
+    RESULT_AND_RECORD: (state: IState, payload: string) => void;
+    CLEAR: (state: IState) => void;
+    CLEAR_ENTRY: (state: IState) => string;
+    REMOVE_ALL_RECORD: (state: IState) => never[];
+};
+
+interface IGetters {
+    processVal: (state: IState) => string;
+    resultVal: (state: IState) => string;
+    recordVal: (state: IState) => IResult[];
+};
+interface IResult { 
+    process: string;
+    result: number | string;
+};
+
+type TCurrentVal = { val: string, filterVal: boolean };
+
+type TArr = Array<TCurrentVal>;
+
+const state: IState = {
     clickVal : '',
     process  : '',
 
@@ -33,14 +56,13 @@ const state: TState = {
         clickPlusMinus : false,
         clickClearEntry: false,
         clickCENumAfterOpe: false,
-        clickOnlyNum : false,
 
         checkCE: false,
         checkInfinityAndNaN: false
     }
 };
 
-let { clickNum, clickOpe, clickEnter, clickPlusMinus, clickClearEntry, clickCENumAfterOpe, clickOnlyNum, checkCE, checkInfinityAndNaN } = state.clickStatus;
+let { clickNum, clickOpe, clickEnter, clickPlusMinus, clickClearEntry, clickCENumAfterOpe, checkCE, checkInfinityAndNaN } = state.clickStatus
 
 const actions : IActions = {
     cal: ({ commit, state }, payload) => {
@@ -52,44 +74,43 @@ const actions : IActions = {
                     return;
                 };
 
+                const callProcess = (): void => {
+                    state.process = '';
+              
+                    commit('PROCESS', val);
+                    state.resultVal = 0;
+                };
+
                 if(clickNum) { // 숫자 클릭 후 연산자 사용 가능, 연산자 클릭 후 또 연산자 클릭 못 하게
                     if(clickClearEntry) { // CE 눌렀을 때
-                        state.resultVal = 0;
-                        state.process = '';
-              
-                        commit('PROCESS', val);
+                        callProcess();
+                        
                         state.clickVal = '';
 
                         clickClearEntry = false;
                     } else { 
                         if(state.resultVal && !clickPlusMinus) { // 결과 값이 있으면서 '+/-'버튼을 클릭했을 때
-                            state.process = '' ;
-              
-                            commit('PROCESS', val);
-                            state.resultVal = 0;
+                            callProcess();
                         } else if(state.resultVal && clickPlusMinus) { // 결과 값이 있으면서 '+/-'버튼을 클릭했을 때
-                            state.process = '' ;
-
-                            commit('PROCESS', val);
-                            state.resultVal = 0;
+                            callProcess();
 
                             clickPlusMinus = false;
                         } else {
-                            if(state.process.split('-')[0] === state.process.split('-')[1] || state.process.includes('*0')) {
-                                state.process = ''
-                            }; // 이전 계산 결과값이 0 이면
+                            if(state.process.split('-')[0] === state.process.split('-')[1] || state.process.includes('*0')) { // 이전 계산 결과값이 0 이면
+                                state.process = '';
+                            }; 
+                            
+                            if(state.process.includes('0*') || state.process.includes('0/')) { // 계산 결과값이 없거나 계산식에 0*, 0/ 이 있으면
+                                state.process = '';
 
-                            if(state.process.includes('0*') || state.process.includes('0/')) {
-                                state.process = ''
-                                state.clickVal = '0'
-                            }; // 계산 결과값이 없거나 계산식에 0*, 0/ 이 있으면 
-                      
+                                state.clickVal = '0';
+                            }; 
+                            
                             if(checkInfinityAndNaN) { // 값이 무한인지 확인
-                                state.process = ''
+                                state.process = '';
 
                                 checkInfinityAndNaN = false;
                             };
-
                             commit('PROCESS', val);
                             state.resultVal = 0;
                         };
@@ -106,8 +127,6 @@ const actions : IActions = {
                 if(val === '.' && (state.clickVal.includes('.') && !clickEnter)) {
                     return;
                 }; // '.' 을 여러번 클릭해도 한번만 입력 되게
-
-                clickNum = true;
                 
                 if(state.clickVal.slice(0, 1) === '0') {
                     state.clickVal = '';
@@ -130,6 +149,7 @@ const actions : IActions = {
                     checkInfinityAndNaN = false;
                 };
 
+                clickNum = true;
                 commit('CLICK_VAL', val);
                 break;
             case 'plusMinus':
@@ -145,56 +165,44 @@ const actions : IActions = {
             case 'enter':
                 checkCE = false;
                 
+                const callResultRecord = (recordOpt?: string): void => {
+                    if(recordOpt) {
+                        commit('RESULT_AND_RECORD', recordOpt);
+                    } else {
+                        commit('RESULT_AND_RECORD');
+                    };
+                };
+
                 if(clickClearEntry) { // CE를 누른 상태
                     if(clickCENumAfterOpe) { // 숫자 클릭 -> 연산자 -> CE -> 숫자 -> 엔터                
-                        commit('RESULT');
-                        commit('RECORD');
-      
-                        state.process  = '' + state.process + state.clickVal;
-                        state.clickVal = '' + state.resultVal;
+                        callResultRecord('addProcessFirst')
                     } else {
-                        if(!Number(state.process.slice(-1))) { // 숫자 클릭 -> CE -> 숫자 -> 엔터
-                            if(state.process.includes('*0')) {
-                                commit('RESULT');
-                                state.process  = state.clickVal + state.process;
-                                commit('RECORD', 'true');
-                            } else {
-                                commit('RESULT');
-                                state.process  = '0 +' + state.process + state.clickVal;
-                                commit('RECORD', 'true');
-                            };
+                        if(!state.process) { // 숫자 클릭 -> CE -> 숫자 -> 엔터
+                            callResultRecord('addZeroFirst');
                         } else { // 숫자 클릭 -> 연산자 -> 숫자 -> 엔터 -> CE -> 숫자 -> 엔터
-                            commit('RESULT');
-                            state.process  = '' + state.clickVal + state.process;
-                            commit('RECORD');
-                        };
-                        state.clickVal = '' + state.resultVal;
+                            callResultRecord('addClickValFirst');
+                        }; 
                     };
-
+                   
                     clickClearEntry = false;
                     clickEnter = true;
                     clickOpe = false;
                 } else {
                     if(clickNum && clickOpe) { // 숫자 클릭 -> 연산자 -> 숫자 클릭 -> 엔터
-                        commit('RESULT');
-                        commit('RECORD');
-                       
-                        state.process  = '' + state.process + state.clickVal;
-                        state.clickVal = '' + state.resultVal;
+                        callResultRecord('addProcessFirst');
+                
                         clickEnter = true;
                         clickOpe = false;
                     };
 
                     if(clickNum && !clickEnter && !clickOpe) { // 숫자만 클릭 -> 엔터
-                        clickOnlyNum = true;
                         state.process = `${state.resultVal} + ${Number(state.clickVal)}`;
-                        commit('RESULT');
-                        commit('RECORD');
-                        state.clickVal = '' + state.resultVal;
+                        callResultRecord();
 
                         clickEnter = true;
                     };
                 };
+                state.clickVal = '' + state.resultVal;
                 break;
             case 'backspace':
                 if(clickNum && !clickEnter) { // 숫자를 누르고 엔터 키를 누르지 않았을 때 (숫자 키를 누르면 clickEnter 초기화 됨 이렇게 안 하면 계산된 값이 지워질 수 있음)
@@ -214,38 +222,38 @@ const actions : IActions = {
     },
 };
 
-const mutations = {
-    'PROCESS': (state: TState, payload: number | string) => {
+const mutations: IMutations = {
+    'PROCESS': (state, payload) => {
         return state.process += state.clickVal + payload;
     },
-    'CLICK_VAL': (state: TState, payload: number | string) => {
+    'CLICK_VAL': (state, payload) => {
         return state.clickVal += payload;
     },
-    'RESULT': (state: TState) => {
-        if(clickClearEntry) {
-            if(clickCENumAfterOpe) {
+    'RESULT_AND_RECORD': (state, payload) => {
+        switch(payload) {
+            case 'addClickValFirst':
+                const sum: string = '' + state.clickVal + state.process;
+
+                state.resultVal = eval(sum);
+                state.record = state.record.concat({process: state.process = sum, result: state.resultVal} as IResult);
+                break;
+            case 'addZeroFirst':
+                const sum2: string = '0+' + state.process + state.clickVal;
+
+                state.resultVal = eval(sum2);
+                state.record = state.record.concat({process: state.process = sum2, result: state.resultVal} as IResult);
+                break;
+            case 'addProcessFirst':
                 clickCENumAfterOpe = false;
-
-                state.resultVal = eval(`${state.process} ${state.clickVal}`);
-            } else {
-                if(!Number(state.process.slice(-1))) {
-                    if(state.process.includes('*0')) {
-                        state.resultVal = eval(state.clickVal + state.process);
-                    } else {
-                        state.resultVal = eval(state.process + state.clickVal);
-                    };
-                } else {
-                    state.resultVal = eval(state.clickVal + state.process);
-                };
-            };
-
-        } else {
-            if(clickOnlyNum){
-                state.resultVal = eval(`${state.process}`);
-                clickOnlyNum = false;
-            } else {
-                state.resultVal = eval(`${state.process} ${state.clickVal}`);
-            };
+                
+                const sum3: string = '' + state.process + state.clickVal;
+                state.resultVal = eval(sum3);
+                state.record = state.record.concat({process: state.process = sum3, result: state.resultVal } as IResult);
+                break;
+            default: // 숫자만 클릭
+                state.resultVal = eval(state.process);
+                state.record = state.record.concat({process: state.process, result: state.resultVal} as IResult);
+                break;   
         };
 
         if(!isFinite(state.resultVal)) { // 결과 값이 무한인지 확인
@@ -254,30 +262,19 @@ const mutations = {
             state.resultVal = 0;
         };
     },
-    'RECORD': (state: TState, payload: boolean) => {
-        if(clickOnlyNum) {
-            return state.record = state.record.concat({process: state.process, result: state.clickVal});
-        } else {
-            if(!clickOpe || payload) {
-                return state.record = state.record.concat({process: state.process, result: state.resultVal});
-            } else {
-                return state.record = state.record.concat({process: state.process + state.clickVal, result: state.resultVal});
-            };
-        };
-    },
-    'CLEAR': (state: TState) => {
+    'CLEAR': state => {
         state.clickVal = '';
         state.process = '';
         state.resultVal = 0;
         
         clickNum = false;
     },
-    'CLEAR_ENTRY': (state: TState) => {
+    'CLEAR_ENTRY': state => {
         let filterVal = true;
 
         if(!checkCE) { // CE가 여러번 눌리지 않게
             checkCE = true;
-            const arr = state.process.split('').reverse().map((val, i) => {
+            const arr: TArr = state.process.split('').reverse().map((val: string, i: number): TCurrentVal => {
                 if (isNaN(Number(val)) && i !== 0) {
                     filterVal = false;
                 };
@@ -285,23 +282,23 @@ const mutations = {
                 return { val, filterVal } ;
             });
 
-            return state.process = arr.reduce((be:any, cur, i) => { // 가장 최근 입력한 값만 남기고 모두 삭제
-                return cur.filterVal || (arr[i-1].filterVal || arr[i-1].val === '.')?
+            return state.process = arr.reduce((be: string[], cur: TCurrentVal, i: number): string[] => { // 가장 최근 입력한 값만 남기고 모두 삭제
+                return cur.filterVal || (arr[i - 1].filterVal || arr[i - 1].val === '.') ?
                      be.concat(cur.val) : be.concat('')
             }, []).reverse().join('');
         } else {
             return state.clickVal = '';
         };
     },
-    'REMOVE_ALL_RECORD': (state: TState) => {
+    'REMOVE_ALL_RECORD': state => {
         return state.record = [];
     },
 };
 
-const getters = {
-    processVal: (state: TState) => state.process,
-    resultVal: (state: TState) => state.clickVal,
-    recordVal: (state: TState) => state.record
+const getters: IGetters = {
+    processVal: state => state.process,
+    resultVal: state => state.clickVal,
+    recordVal: state => state.record
 };
 
 export default {
