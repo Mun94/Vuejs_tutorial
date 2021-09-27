efb2308db5719f72a854cf4f5640a7b52006256e
Mun94 <seongwoo720@gmail.com>
2021-09-27 19:48:31 +0900

    test

---
+++ test-ts-setup/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>{{ computedMsg }}</button>
+  {{ text }}
+</template>
+
+<script setup lang = "ts">
+import { computed, defineProps } from 'vue'
+
+const props = defineProps({
+    msg: {}
+});
+
+const text: any = '123';
+
+const computedMsg = computed(() => props.msg + '!!!')
+</script>
---
+++ test-ts-setup/src/components/TestM.vue
@@ -0,0 +1,18 @@
+<template>
+    <Test v-bind:msg = "testprops" v-on:test-func = "testFunc"/>
+</template>
+
+<script>
+    export default {
+        name: 'TestM'
+    }
+</script>
+
+<script setup>
+    import { ref, Ref } from 'vue';
+    import Test from './Test.vue';
+
+    const testprops = ref(0);
+
+    const testFunc = () => ++testprops.value
+</script>
