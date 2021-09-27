a294db6a032e9a7542f241a3390586386f95d82f
Mun94 <seongwoo720@gmail.com>
2021-09-27 11:22:20 +0900

    add: TodoListContainer

---
+++ 한 권으로 배우는 vue.js 3 책/chapter-04/todolist/src/components/TodoListContainer.vue
@@ -0,0 +1,29 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: 'TodoListContainer'
+    }
+</script>
+
+<script setup>
+    import { ref, readonly, provide } from 'vue';
+
+    const todos = ref([]);
+
+    const initTodos = (init_todos) => {
+        todos.value = init_todos;
+    };
+
+    const addTodo = (todo, date) => {};
+    const removeTodo = (id) => {};
+    const completeTodo = (id) => {};
+
+    provide('todos', readonly(todos));
+    provide('addTodo', addTodo);
+    provide('removeTodo', removeTodo);
+    provide('completeTodo', completeTodo);
+</script>
+
