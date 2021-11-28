cc53cdfd3b403e333b5aacd41910f28fbdd88278
Mun94 <seongwoo720@gmail.com>
2021-11-28 19:15:04 +0900

    add: 네트워크 에러 확인 방법과 에러 처리 코드 구현

---
+++ 실무에 필요한 모든 것/vue-til/src/components/LoginForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+    <p>{{ logMessage }}</p>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api/index';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      logMessage: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+
+        const { data } = await loginUser(userData);
+        this.logMessage = `${data.user.username}님 환영합니다.`;
+      } catch (error) {
+        console.log(error.response);
+      }
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+    },
+  },
+};
+</script>
+
+<style></style>
